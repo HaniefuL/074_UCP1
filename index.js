57654f39cd4624a1b8249ef3c5fd5886d9517e0e
HaniefuL <haniefulgeofarry@gmail.com>
2025-11-03 09:33:02 +0700

    menambahkan app.put

--- index.js
+++ index.js
@@ -39,3 +39,20 @@ db.sequelize.sync()
             res.send(err);
         }
     });
+
+    app.put('/buku/:id', async (req, res) => {
+        const id = req.params.id;
+        const data = req.body;
+
+        try {
+            const buku = await db.Buku.findByPk(id);
+            if (!buku) {
+                return res.status(404).send({ message: 'buku tidak ditemukan' });
+            }
+
+            await buku.update(data);
+            res.send({ message: 'buku berhasil diupdate', buku });
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    });
